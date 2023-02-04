71cf9fe6626e6f0ac5b89c20d3e2ef353a1e58c6
I Nyoman Jyotisa <jyotisa1616@gmail.com>
2023-02-04 14:01:38 +0800

    init

---
+++ storage/framework/views/4d449a69b526c79cc92740ad8390b0f2a0e61bed.php
@@ -0,0 +1,71 @@
+<?php $__env->startSection('title'); ?> Jenis Biota  <?php $__env->stopSection(); ?>
+<?php $__env->startSection('css'); ?>
+<link href="<?php echo e(URL::asset('assets/libs/datatables.net-bs4/datatables.net-bs4.min.css')); ?>" rel="stylesheet" type="text/css" />
+<link href="<?php echo e(URL::asset('assets/libs/datatables.net-buttons-bs4/datatables.net-buttons-bs4.min.css')); ?>" rel="stylesheet" type="text/css" />
+<link href="<?php echo e(URL::asset('assets/libs/datatables.net-responsive-bs4/datatables.net-responsive-bs4.min.css')); ?>" rel="stylesheet" type="text/css" />
+
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection('content'); ?>
+<?php $__env->startComponent('components.breadcrumb'); ?>
+<?php $__env->slot('li_1'); ?> U-Sea <?php $__env->endSlot(); ?>
+<?php $__env->slot('title'); ?> Jenis Biota <?php $__env->endSlot(); ?>
+<?php echo $__env->renderComponent(); ?>
+
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <div class="row">
+                    <div class="col-10">
+                        <h4 class="card-title">Jenis Biota</h4>
+                        <p class="card-title-desc">Ini jenis biota</p>
+                    </div>
+                    <div class="col-2 text-right">
+                        <a href="jenis-biota/create"><button type="button" class="mt-1 btn btn-primary waves-effect waves-light">Tambah Data</button></a>
+                    </div>
+                </div>
+                </div>
+            <div class="card-body">
+
+                <table id="datatable" class="table table-bordered dt-responsive  nowrap w-100">
+                    <thead>
+                    <tr>
+                        <th class="col-10">Jenis Biota</th>
+                        <th class="col-2">Action</th>
+                    </tr>
+                    </thead>
+
+
+                    <tbody>
+                    <?php $__currentLoopData = $jenisBiotas; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $jenisBiota): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                    <tr>
+                        <td><?php echo e($jenisBiota->jenis_biota); ?></td>
+                        <td>
+                            <a href="jenis-biota/edit/<?php echo e($jenisBiota->id); ?>"><button type="button" class="mt-1 btn btn-warning waves-effect waves-light">Edit</button></a>
+                            <a onclick="return confirm ('Hapus data?')" href="jenis-biota/destroy/<?php echo e($jenisBiota->id); ?>"><button type="button" class="mt-1 btn btn-danger waves-effect waves-light">Hapus</button></a>
+                        </td>
+                    </tr>
+                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                    </tbody>
+                </table>
+
+            </div>
+        </div>
+    </div> <!-- end col -->
+</div> <!-- end row -->
+
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection('script'); ?>
+<script src="<?php echo e(URL::asset('assets/libs/datatables.net/datatables.net.min.js')); ?>"></script>
+<script src="<?php echo e(URL::asset('assets/libs/datatables.net-bs4/datatables.net-bs4.min.js')); ?>"></script>
+<script src="<?php echo e(URL::asset('assets/libs/datatables.net-buttons/datatables.net-buttons.min.js')); ?>"></script>
+<script src="<?php echo e(URL::asset('assets/libs/datatables.net-buttons-bs4/datatables.net-buttons-bs4.min.js')); ?>"></script>
+<script src="<?php echo e(URL::asset('assets/libs/jszip/jszip.min.js')); ?>"></script>
+<script src="<?php echo e(URL::asset('assets/libs/pdfmake/pdfmake.min.js')); ?>"></script>
+<script src="<?php echo e(URL::asset('assets/libs/datatables.net-responsive/datatables.net-responsive.min.js')); ?>"></script>
+<script src="<?php echo e(URL::asset('assets/libs/datatables.net-responsive-bs4/datatables.net-responsive-bs4.min.js')); ?>"></script>
+<script src="<?php echo e(URL::asset('assets/js/pages/datatables.init.js')); ?>"></script>
+<script src="<?php echo e(URL::asset('assets/js/app.min.js')); ?>"></script>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\job\dashboardusea3\resources\views/jenis-biota/index.blade.php ENDPATH**/ ?>
